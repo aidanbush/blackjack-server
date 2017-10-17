0612a7f53d95e7380a3921693d09cc212691d320
aidan <aidandbush@gmail.com>
2017-10-17 10:20:52 -0600

    initial commit add template files

---
+++ template.c
@@ -0,0 +1,13 @@
+/* Author: Aidan Bush
+ * Assign: Assign 1
+ * Course: CMPT 361
+ * Date:
+ * File:
+ * Description:
+ */
+
+/* standard libraries */
+
+/* system libraries */
+
+/* project includes */
---
+++ template.h
@@ -0,0 +1,12 @@
+/* Author: Aidan Bush
+ * Assign: Assign 1
+ * Course: CMPT 361
+ * Date:
+ * File:
+ * Description:
+ */
+
+#ifndef _H
+#define _H
+
+#endif /* _H */
